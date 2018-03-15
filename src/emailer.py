ac39e052e593b802de7b23a141c60131cbb6563c
Aram Simonian <aramsimonian@Arams-MacBook-Pro.local>
2018-03-15 15:25:45 +0000

    Changed format of email file to include name; altered test accordingly

--- src/emailer.py
+++ src/emailer.py
@@ -1,14 +1,15 @@
 
 def get_email_address(email_filename):
 
-    emails = []
+    emails = {}
     try:
 
         email_file = open(email_filename.name, 'r')
 
         for line in email_file:
-            emails.append(line.strip())
+            (email, name) = line.split(',')
+            emails[email] = name.strip()
     except FileNotFoundError as err:
         print(err)
-    
+
     return emails
--- tests/test_email_file.py
+++ tests/test_email_file.py
@@ -6,7 +6,8 @@ import unittest
 class TestEmailMethods(unittest.TestCase):
     TEST_FILE = None
     TEST_FILE_NAME = 'emails.txt'
-    TEST_CONTENT = 'bob.smith@company.com\nbobby.joe@southern.com\n'
+    TEST_CONTENT = 'bob.smith@company.com,Bob Smith\nbobby.joe@southern.com,Bobby Joe\n'
+    TEST_EMAIL_RESULT = {'bob.smith@company.com':'Bob Smith','bobby.joe@southern.com':'Bobby Joe'}
 
     def create_email_file(self):
         test_file = open(self.TEST_FILE_NAME, 'w+')
@@ -16,4 +17,7 @@ class TestEmailMethods(unittest.TestCase):
         return test_file
 
     def test_read_email_address(self):
-        self.assertEqual(self.TEST_CONTENT.splitlines(), emailer.get_email_address(self.create_email_file()))
+        # print(self.TEST_CONTENT.splitlines())
+        # print(emailer.get_email_address(self.create_email_file()))
+        self.assertEqual(self.TEST_EMAIL_RESULT
+            , emailer.get_email_address(self.create_email_file()))
